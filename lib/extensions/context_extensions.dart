65cd65e8686317fe039b06da8df343162c3b77cc
Saad Akram <saadakram200@gmail.com>
2025-08-12 12:22:04 +0500

    added theme extensions

--- lib/extensions/context_extensions.dart
+++ lib/extensions/context_extensions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension ContextUtilities on BuildContext {
+  // Screen dimensions & paddings
   double get width => MediaQuery.of(this).size.width;
   double get height => MediaQuery.of(this).size.height;
   EdgeInsets get padding => MediaQuery.of(this).padding;
@@ -9,16 +10,23 @@ extension ContextUtilities on BuildContext {
   double get leftPadding => MediaQuery.of(this).padding.left;
   double get rightPadding => MediaQuery.of(this).padding.right;
 
+  // Theme & Styles
+  ColorScheme get colors => Theme.of(this).colorScheme;
+  TextTheme get textStyles => Theme.of(this).textTheme;
+
+  // Navigation helpers
   Future<dynamic> push(
       {required Widget child, bool rootNavigator = false}) async {
     return await Navigator.of(this, rootNavigator: rootNavigator)
         .push(MaterialPageRoute(builder: (ctx) => child));
   }
+
   Future<dynamic> pushReplacement(
       {required Widget child, bool rootNavigator = false}) async {
     return await Navigator.of(this, rootNavigator: rootNavigator)
         .pushReplacement(MaterialPageRoute(builder: (ctx) => child));
   }
+
   Future<dynamic> pushAndRemoveUntil(
       {required Widget child, bool rootNavigator = false}) async {
     return await Navigator.of(this, rootNavigator: rootNavigator)
@@ -27,9 +35,11 @@ extension ContextUtilities on BuildContext {
       (route) => false,
     );
   }
+
   bool canPop() => Navigator.of(this).canPop();
+
   void pop({dynamic data, bool rootNavigator = false}) {
-    if(canPop()){
+    if (canPop()) {
       Navigator.of(this, rootNavigator: rootNavigator).pop(data);
     }
   }
